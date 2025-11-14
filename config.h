fd75f8765ac052106729575d55002e4a1019c3d4
Marcus Janum <dsx992@alumni.ku.dk>
2025-11-14 16:38:21 +0100

    no borders

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #define TERMINAL "alacritty"
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
